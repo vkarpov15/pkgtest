4f2e0704faa2babce68d4eefbd09414fdfd2fdc3
Valeri Karpov <valkar207@gmail.com>
2017-05-03 15:42:56 -0600

    semicolons on semicolons

--- src/api.js
+++ src/api.js
@@ -4,28 +4,28 @@ const co = require('co');
 const express = require('express');
 
 module.exports = db => {
-  const router = express.Router()
+  const router = express.Router();
 
   const wrapAsync = handler => (req, res) => handler(req)
     .then(result => res.json(result))
-    .catch(error => res.status(500).json({ error: error.message }))
+    .catch(error => res.status(500).json({ error: error.message }));
 
   router.get('/', wrapAsync(async function(req) {
-    return db.collection('Book').find().sort({ createdAt: -1 }).toArray()
-  }))
+    return db.collection('Book').find().sort({ createdAt: -1 }).toArray();
+  }));
 
   router.post('/', wrapAsync(async function(req) {
-    const book = new BookType(req.body)
-    await db.collection('Book').insertOne(book)
-    return { book }
+    const book = new BookType(req.body);
+    await db.collection('Book').insertOne(book);
+    return { book };
   }))
 
   router.delete('/:id', wrapAsync(async function(req) {
     const { result } = await db.collection('Book').deleteOne({
       _id: Archetype.to(req.params.id, ObjectId)
-    })
-    return { result }
-  }))
+    });
+    return { result };
+  }));
 
-  return router
-}
+  return router;
+};
